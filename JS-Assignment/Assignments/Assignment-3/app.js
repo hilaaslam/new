9ac5390f1d9a0c2a464098de19b01e37ee40279c
Hila Aslam <hilaaslam157@gmail.com>
2023-05-25 12:21:58 +0500

    still working

---
+++ JS-Assignment/Assignments/Assignment-3/app.js
@@ -0,0 +1,41 @@
+// // 1. Declare a variable called age & assign to it your age. Show
+// // your age in an alert box.
+
+// var age = ("I am 16 years old")
+// alert (age);
+
+
+// // 2. Declare & initialize a variable to keep track of how many
+// // times a visitor has visited a web page. Show his/her
+// // number of visits on your web page. For example: “You
+// // have visited this site N times”.
+
+// var visitPage = ("13")
+// visitPage++
+// alert("you have visited this site "+visitPage+ " times");
+
+
+// // 3. Declare a variable called birthYear & assign to it your
+// // birth year. Show the following message in your browser:
+
+// var birthYear = "<br><br> My birth year is 2006.<br> Data type of my ddeclared variable is number."
+// document.write (birthYear);
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var visitorsName ="Jhone Doe";
+// var productTitle ="T-shirt(s)";
+// var productQuant ="5";
+// document.write ("<br><br>"+visitorsName+ " ordered " +productQuant+" "+productTitle+ " on XYZ Clothing store.");
+
+
+                                                                  // // -- END --
---
+++ JS-Assignment/Assignments/Assignment-4/app.js
@@ -0,0 +1,38 @@
+// 1. Declare 3 variables in one statement.
+
+var year = "1995", month = "12", date = "30";
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// LEGAL VARIABLES
+
+// 1. myname
+// 2. myName
+// 3. my_name
+// 4. my9ame
+// 5. MyName
+
+// ILLEGAL VARIABLE
+
+// 1. 365days (starts with a digit)
+// 2. my-name (contains a hyphen)
+// 3. 157 (it is a number)
+// 4. var (reserved keyword)
+// 5. my name (contain space)
+
+
+// // 3. Display this in your browser
+// // a) A heading stating “Rules for naming JS variables”
+// // b) Variable names can only contain ______, ______,
+// // ______ and ______.
+// // For example $my_1stVariable
+// // c) Variables must begin with a ______, ______ or
+// // _____. For example $name, _name or name
+// // d) Variable names are case _________
+// // e) Variable names should not be JS _________ 
+
+              //  ANSWER IS IN index.html FILE.....
+
+
+                                                                  // // -- END --
